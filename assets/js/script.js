a876eed6beafbc89b3e9dc3721176c4523fc9a69
bethanyalicehall <bethany.hall66@gmail.com>
2022-10-02 11:30:10 +0000

    Added functin to start the game and to display total moves and the time elapsed

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,3 +70,14 @@ const generateGame = () => {
 }
 
 
+const startGame = () => {
+    state.gameStarted = true
+    selectors.start.classList.add('disabled')
+
+    state.loop = setInterval(() => {
+        state.totalTime++
+
+        selectors.moves.innerText = `${state.totalFlips} moves`
+        selectors.timer.innerText = `time: ${state.totalTime} sec`
+    }, 1000)
+}
