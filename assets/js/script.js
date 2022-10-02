50ff8bc5e55319a3ce701d5f9668e5cd9baa791d
bethanyalicehall <bethany.hall66@gmail.com>
2022-10-02 11:16:56 +0000

    Added function to shuffle cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,31 @@ const state = {
     totalTime: 0,
     loop: null
 }
+
+const shuffle = array => {
+    const clonedArray = [...array]
+
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index + 1))
+        const original = clonedArray[index]
+
+        clonedArray[index] = clonedArray[randomIndex]
+        clonedArray[randomIndex] = original
+    }
+
+    return clonedArray
+}
+
+const pickRandom = (array, items) => {
+    const clonedArray = [...array]
+    const randomPicks = []
+
+    for (let index = 0; index < items; index++) {
+        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        
+        randomPicks.push(clonedArray[randomIndex])
+        clonedArray.splice(randomIndex, 1)
+    }
+
+    return randomPicks
+}
