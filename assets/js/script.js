8bc34a155d8d762bf9c621e08efd83a3f18a2252
bethanyalicehall <bethany.hall66@gmail.com>
2022-10-02 16:11:15 +0000

    Corrected spelling mistakes and formatted, also added starfish image

--- assets/js/script.js
+++ assets/js/script.js
@@ -58,7 +58,7 @@ const generateGame = () => {
         throw new Error("The dimension of the board must be an even number.")
     }
 
-    const emojis = ['🐶', '🦦 ', '🐯', '🐘', '🐧', '🐒 ', '🦙', '🦒', '🐬', '🦜']
+    const emojis = ['🐶', '🦦 ', '🐯', '🐘', '🐧', '🐒 ', '🦙', '🦒', '🐬', '🦜', '🦋', '🐢', '🦁', '🦧', '🦓', '🐠', '🦥', '🐝']
     const picks = pickRandom(emojis, (dimensions * dimensions) / 2) 
     const items = shuffle([...picks, ...picks])
     const cards = `
