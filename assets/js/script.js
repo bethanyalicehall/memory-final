06dbe8174873f908608299929b5a3b8bdbff2326
bethanyalicehall <bethany.hall66@gmail.com>
2022-10-07 19:10:08 +0000

    Added further comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
+//Fact card page
 const fcards = document.querySelectorAll(".fact-card");
+//Function to flip fact card when the user clicks the image
 function flipTheCard() {
     this.classList.toggle("flip");
   }
   fcards.forEach(fcard => fcard.addEventListener("click", flipTheCard));
 
+//Level 1 and level 2 pages 
 
+//The following code was adapted from webtips.dev, some of the code is directly taken and some has been adapted for my game - see credits section of README.md for further details on this.
+
+//Selectors to simplify script
 const selectors = {
     boardContainer: document.querySelector(".board-container"),
     board: document.querySelector(".board"),
@@ -14,14 +20,16 @@ const selectors = {
     win: document.querySelector(".win")
 };
 
+//Setting the pre-game state of the game board including the cards, move counter and timer.
 const state = {
-    gameStarted: false,
-    flippedCards: 0,
+    gameBegun: false,
     totalFlips: 0,
     totalTime: 0,
+    flippedCards: 0,
     loop: null
 };
 
+//Shuffling the array to make sure each game has a different set/arrangement of cards
 const shuffle = array => {
     const clonedArray = [...array];
 
@@ -35,29 +43,29 @@ const shuffle = array => {
 
     return clonedArray;
 };
-
+// Picking random cards out of the shuffled array 
 const pickRandom = (array, items) => {
     const clonedArray = [...array];
-    const randomPicks = [];
+    const randomSelects = [];
 
     for (let index = 0; index < items; index++) {
         const randomIndex = Math.floor(Math.random() * clonedArray.length);
 
-        randomPicks.push(clonedArray[randomIndex]);
+        randomSelects.push(clonedArray[randomIndex]);
         clonedArray.splice(randomIndex, 1);
     }
 
-    return randomPicks;
+    return randomSelects;
 };
 
+// Generating the game board, each possible animal emoji is included below, the size of the card is taken from the level1 and level2 html, this is then multiplied by 2 to produce the game board dimensions.
 const generateGame = () => {
-    const dimensions = selectors.board.getAttribute("data-dimension");
-
-    const emojis = ["🐶", "🦦", "🐯", "🐘", "🐧", "🐒", "🦙", "🦒", "🐬", "🦜", "🦋", "🐢", "🦁", "🦧", "🦓", "🐠", "🦥", "🐝"]
-    const picks = pickRandom(emojis,(dimensions * 2));
-    const items = shuffle([...picks, ...picks]);
+    const sizes = selectors.board.getAttribute("data-dimension");
+    const animals = ["🐶", "🦦", "🐯", "🐘", "🐧", "🐒", "🦙", "🦒", "🐬", "🦜", "🦋", "🐢", "🦁", "🦧", "🦓", "🐠", "🦥", "🐝"]
+    const selects = pickRandom(animals,(sizes * 2));
+    const items = shuffle([...selects, ...selects]);
     const cards = `
-        <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
+        <div class="board" style="grid-template-columns: repeat(${sizes}, auto)">
             ${items.map(item => `
                 <div class="card">
                     <div class="card-front"></div>
@@ -72,9 +80,9 @@ const generateGame = () => {
     selectors.board.replaceWith(parser.querySelector(".board"));
 
 };
-
+// Code to run when the game has begun and the timer will start and moves will begin to be incremented
 const startGame = () => {
-    state.gameStarted = true;
+    state.gameBegun = true;
     selectors.start.classList.add("disabled");
 
     state.loop = setInterval(() => {
@@ -84,6 +92,8 @@ const startGame = () => {
         selectors.timer.innerText = `time: ${state.totalTime} sec`;
     }, 1000);
 };
+
+// This is to flip cards back over if the two selected cards do not match
 const flipBackCards = () => {
     document.querySelectorAll(".card:not(.matched)").forEach(card => {
         card.classList.remove("flipped");
@@ -91,12 +101,13 @@ const flipBackCards = () => {
 
     state.flippedCards = 0;
 };
-
+// This is to ensure no more than two cards can be flipped over at one time
+// Also this makes the cards that do match stay face up
 const flipCard = card => {
     state.flippedCards++;
     state.totalFlips++;
 
-    if (!state.gameStarted) {
+    if (!state.gameBegun) {
         startGame();
     }
 
@@ -116,6 +127,7 @@ const flipCard = card => {
             flipBackCards();
         }, 1000);
     }
+    //This displays the win message when there are no further cards to be flipped over and all of the matches have been found. The stats are inputted within the HTML below.
     if (!document.querySelectorAll(".card:not(.flipped)").length) {
         setTimeout(() => {
             selectors.boardContainer.classList.add("flipped");
@@ -135,6 +147,7 @@ const flipCard = card => {
     }
 };
 
+// Event listners for card flipping
 const attachEventListeners = () => {
     document.addEventListener("click", event => {
         const eventTarget = event.target;
@@ -148,6 +161,6 @@ const attachEventListeners = () => {
     });
 };
 
+//Calling functions to generate the game once the game has begun and to attach the click event listeners
 generateGame();
 attachEventListeners();
-///////
