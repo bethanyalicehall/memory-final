b359441180bd48e880fc9b34ffbb14e657920124
bethanyalicehall <bethany.hall66@gmail.com>
2022-10-07 16:15:24 +0000

    Updated JS file after running it through JSHint

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ const selectors = {
     timer: document.querySelector(".timer"),
     start: document.querySelector("button"),
     win: document.querySelector(".win")
-}
+};
 
 const state = {
     gameStarted: false,
@@ -20,42 +20,42 @@ const state = {
     totalFlips: 0,
     totalTime: 0,
     loop: null
-}
+};
 
 const shuffle = array => {
-    const clonedArray = [...array]
+    const clonedArray = [...array];
 
     for (let index = clonedArray.length - 1; index > 0; index--) {
-        const randomIndex = Math.floor(Math.random() * (index + 1))
-        const original = clonedArray[index]
+        const randomIndex = Math.floor(Math.random() * (index + 1));
+        const original = clonedArray[index];
 
-        clonedArray[index] = clonedArray[randomIndex]
-        clonedArray[randomIndex] = original
+        clonedArray[index] = clonedArray[randomIndex];
+        clonedArray[randomIndex] = original;
     }
 
-    return clonedArray
-}
+    return clonedArray;
+};
 
 const pickRandom = (array, items) => {
-    const clonedArray = [...array]
-    const randomPicks = []
+    const clonedArray = [...array];
+    const randomPicks = [];
 
     for (let index = 0; index < items; index++) {
-        const randomIndex = Math.floor(Math.random() * clonedArray.length)
+        const randomIndex = Math.floor(Math.random() * clonedArray.length);
 
-        randomPicks.push(clonedArray[randomIndex])
-        clonedArray.splice(randomIndex, 1)
+        randomPicks.push(clonedArray[randomIndex]);
+        clonedArray.splice(randomIndex, 1);
     }
 
-    return randomPicks
-}
+    return randomPicks;
+};
 
 const generateGame = () => {
-    const dimensions = selectors.board.getAttribute("data-dimension")
+    const dimensions = selectors.board.getAttribute("data-dimension");
 
     const emojis = ["🐶", "🦦", "🐯", "🐘", "🐧", "🐒", "🦙", "🦒", "🐬", "🦜", "🦋", "🐢", "🦁", "🦧", "🦓", "🐠", "🦥", "🐝"]
-    const picks = pickRandom(emojis,(dimensions * 2))
-    const items = shuffle([...picks, ...picks])
+    const picks = pickRandom(emojis,(dimensions * 2));
+    const items = shuffle([...picks, ...picks]);
     const cards = `
         <div class="board" style="grid-template-columns: repeat(${dimensions}, auto)">
             ${items.map(item => `
@@ -65,60 +65,60 @@ const generateGame = () => {
                 </div>
             `).join('')}
        </div>
-    `
+    `;
     
-    const parser = new DOMParser().parseFromString(cards, "text/html")
+    const parser = new DOMParser().parseFromString(cards, "text/html");
 
-    selectors.board.replaceWith(parser.querySelector(".board"))
+    selectors.board.replaceWith(parser.querySelector(".board"));
 
-}
+};
 
 const startGame = () => {
-    state.gameStarted = true
-    selectors.start.classList.add("disabled")
+    state.gameStarted = true;
+    selectors.start.classList.add("disabled");
 
     state.loop = setInterval(() => {
-        state.totalTime++
+        state.totalTime++;
 
-        selectors.moves.innerText = `${state.totalFlips} moves`
-        selectors.timer.innerText = `time: ${state.totalTime} sec`
-    }, 1000)
-}
+        selectors.moves.innerText = `${state.totalFlips} moves`;
+        selectors.timer.innerText = `time: ${state.totalTime} sec`;
+    }, 1000);
+};
 const flipBackCards = () => {
     document.querySelectorAll(".card:not(.matched)").forEach(card => {
-        card.classList.remove("flipped")
-    })
+        card.classList.remove("flipped");
+    });
 
-    state.flippedCards = 0
-}
+    state.flippedCards = 0;
+};
 
 const flipCard = card => {
-    state.flippedCards++
-    state.totalFlips++
+    state.flippedCards++;
+    state.totalFlips++;
 
     if (!state.gameStarted) {
-        startGame()
+        startGame();
     }
 
     if (state.flippedCards <= 2) {
-        card.classList.add("flipped")
+        card.classList.add("flipped");
     }
 
     if (state.flippedCards === 2) {
-        const flippedCards = document.querySelectorAll(".flipped:not(.matched)")
+        const flippedCards = document.querySelectorAll(".flipped:not(.matched)");
 
         if (flippedCards[0].innerText === flippedCards[1].innerText) {
-            flippedCards[0].classList.add("matched")
-            flippedCards[1].classList.add("matched")
+            flippedCards[0].classList.add("matched");
+            flippedCards[1].classList.add("matched");
         }
 
         setTimeout(() => {
-            flipBackCards()
-        }, 1000)
+            flipBackCards();
+        }, 1000);
     }
     if (!document.querySelectorAll(".card:not(.flipped)").length) {
         setTimeout(() => {
-            selectors.boardContainer.classList.add("flipped")
+            selectors.boardContainer.classList.add("flipped");
             selectors.win.innerHTML = `
                 <span class="win-text">
                     <h1 class="font-1">You won!</h1>
@@ -128,26 +128,26 @@ const flipCard = card => {
                     <br>
                     <button class="win-btn game-btn"><a href="level1.html">Practice more on level 1</a></button>
                 </span>
-            `
+            `;
 
-            clearInterval(state.loop)
-        }, 1000)
+            clearInterval(state.loop);
+        }, 1000);
     }
-}
+};
 
 const attachEventListeners = () => {
     document.addEventListener("click", event => {
-        const eventTarget = event.target
-        const eventParent = eventTarget.parentElement
+        const eventTarget = event.target;
+        const eventParent = eventTarget.parentElement;
 
         if (eventTarget.className.includes("card") && !eventParent.className.includes("flipped")) {
-            flipCard(eventParent)
+            flipCard(eventParent);
         } else if (eventTarget.nodeName === "BUTTON" && !eventTarget.className.includes("disabled")) {
-            startGame()
+            startGame();
         }
-    })
-}
+    });
+};
 
-generateGame()
-attachEventListeners()
+generateGame();
+attachEventListeners();
 ///////
